97b0317b4ae0ee8db11b15b0aba35d7b5f2b85d4
hrutkabence <48557905+hrutkabence@users.noreply.github.com>
2022-03-09 12:07:59 +0100

    Update pc2dem.py
    New parameters.
    Point cloud (ground and non-ground points) export

--- python/pc2dem.py
+++ python/pc2dem.py
@@ -1,7 +1,12 @@
 """
-    generate Digital Elevation Model from a point cloud using
-    Cloth Simulation Filter and GDAL
+    generate Digital Elevation Model (DEM) from a point cloud using Cloth Simulation Filter (CSF) and GDAL
+
+    W. Zhang, J. Qi*, P. Wan, H. Wang, D. Xie, X. Wang, and G. Yan, 
+    “An Easy-to-Use Airborne LiDAR Data Filtering Method Based on Cloth Simulation,”
+    Remote Sens., vol. 8, no. 6, p. 501, 2016. (http://www.mdpi.com/2072-4292/8/6/501/htm)
+
 """
+from email.mime import base
 from os import path, remove
 import argparse
 import CSF
@@ -15,35 +20,42 @@ parser.add_argument('name', metavar='file_name', type=str, nargs=1,
                     help='point cloud to process')
 parser.add_argument('-r', '--resolution', type=float, default=2.0,
                     help='resolution for dem')
-parser.add_argument('-o', '--output', type=str,
+parser.add_argument('-o', '--output', type=str, default='output',
                     help='output DEM file and non-ground points')
 parser.add_argument('--rigidness', type=int, default=3,
-                    help='rigidness of cloth 1 or 2 or 3')
+                    help='rigidness of cloth 1,2,3: mountain with desen vegetation(1) OR complex scenes(2) OR flat terrain with high-rise buildings(3)')
 parser.add_argument('--smooth', action='store_true',
                     help='postprocess to smooth')
 parser.add_argument('--iterations', type=int, default=500,
                     help='number of iterations')
+parser.add_argument('--classification', type=int, default=0.5,
+                    help='classification threshold')
 args = parser.parse_args()
+
 # load PC
 pc = o3d.io.read_point_cloud(args.name[0])
 pc_xyz = np.asarray(pc.points)
+
 # CSF parameters TODO
 csf = CSF.CSF()
 csf.params.bSloopSmooth = args.smooth
 csf.params.cloth_resolution = args.resolution / 2.0
 csf.params.rigidness = args.rigidness
 csf.params.time_step = 0.65
-csf.params.class_threshold = 0.5
+csf.params.class_threshold = args.classification
 csf.params.interations = args.iterations
 
 csf.setPointCloud(pc_xyz)
 ground = CSF.VecInt()
 non_ground = CSF.VecInt()
-csf.do_filtering(ground, non_ground)
+csf.do_filtering(ground, non_ground, exportCloth=False) 
+
 # write ground points into ascii with vrt
-base_name = path.splitext(args.output)[0]
+base_name = path.splitext(args.output)[0]                      
+
 name = path.basename(base_name)
 pc_ground = pc.select_by_index(ground)
+
 #o3d.io.write_point_cloud(base_name + '.csv', pc_ground, True)
 xyz = np.asarray(pc_ground.points)
 df = pd.DataFrame(xyz, columns=['x', 'y', 'z'])
@@ -57,11 +69,17 @@ with open(vrt, "w") as fvrt:
     <GeometryField encoding="PointFromColumns" x="x" y="y" z="z"/>
 </OGRVRTLayer>
 </OGRVRTDataSource>""")
+
 # write non-ground points into binary
 non_ground = pc.select_by_index(non_ground)
-o3d.io.write_point_cloud(base_name + '.ply', non_ground, True)
-# create DEM
-gdal.Grid(base_name + '.tif', vrt)
+o3d.io.write_point_cloud(base_name + '_non_ground.ply', non_ground, True)
+# write ground points into binary 
+ground = pc.select_by_index(ground)
+o3d.io.write_point_cloud(base_name + '_ground.ply', ground, True)
+
+# create DEM                                                        
+GDAL.Grid(base_name + '.tif', vrt)              
+
 # remove temperary files
 remove(vrt)
 remove(base_name + '.csv')

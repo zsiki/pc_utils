57bf18d79efe88b436e4bb62a132a1a54dcd8613
zsiki <siki1958@gmail.com>
2022-05-06 18:02:34 +0200

    restucture code, error handling added

--- python/pc2ndsm.py
+++ python/pc2ndsm.py
@@ -6,10 +6,41 @@ from osgeo import gdal
 import open3d as o3d
 import numpy as np
 
+def ndsm(src_ds, pc):
+    """ Create nDSM from DEM and point cloud, points outside the DEM or
+        in a NODATA cell are dropped, the oroginal point clouds points are
+        changed
+
+        :param src_ds: DEM (GDAL)
+        :param pc: point cloud
+    """
+    dem_max_col = src_ds.RasterXSize - 1
+    dem_max_row = src_ds.RasterYSize - 1
+    gt = src_ds.GetGeoTransform()
+    rb = src_ds.GetRasterBand(1)
+    dem = rb.ReadAsArray()
+    # get point cloud
+    pc_xyz = np.asarray(pc.points)
+    outlier = []
+    i = -1
+    for xyz in pc_xyz:
+        i += 1
+        #Convert from map coordinates to grid indices
+        px = min(max(int((xyz[0] - gt[0]) / gt[1]), 0), dem_max_col) #x pixel
+        py = min(max(int((xyz[1] - gt[3]) / gt[5]), 0), dem_max_row) #y pixel
+        dz = xyz[2] - dem[py, px]
+        # filter NODATA and low vegetation
+        if dem[py, px] == DEM_NODATA or dz < ndsm_min:
+            outlier.append(i)
+            continue    # skip nodata 
+        xyz[2] = dz
+    if len(outlier) > 0:
+        pc = pc.select_by_index(outlier, invert=True)
+
 DEM_NODATA = 0
 if len(sys.argv) < 3:
     print("usage: {} dem_file point_cloud min". format(sys.argv[0]))
-    sys.exit()
+    sys.exit(3)
 dem_filename = sys.argv[1]
 pc_filename = sys.argv[2]
 ndsm_min = -9999.
@@ -19,28 +50,14 @@ pc_split = os.path.splitext(pc_filename)
 out_filename = pc_split[0] + '_ndsm' + pc_split[1]
 # get DEM
 src_ds = gdal.Open(dem_filename)
-dem_max_col = src_ds.RasterXSize - 1
-dem_max_row = src_ds.RasterYSize - 1
-gt = src_ds.GetGeoTransform()
-rb = src_ds.GetRasterBand(1)
-dem = rb.ReadAsArray()
+if not src_ds:
+    print(f'Cannot open DEM {dem_filename}')
+    sys.exit(1)
 # get point cloud
 pc = o3d.io.read_point_cloud(pc_filename)
-pc_xyz = np.asarray(pc.points)
-outlier = []
-i = -1
-for xyz in pc_xyz:
-    i += 1
-    #Convert from map to grid indices
-    px = min(max(int((xyz[0] - gt[0]) / gt[1]), 0), dem_max_col) #x pixel
-    py = min(max(int((xyz[1] - gt[3]) / gt[5]), 0), dem_max_row) #y pixel
-    dz = xyz[2] - dem[py, px]
-    # filter NODATA and low vegetation
-    if dem[py, px] == DEM_NODATA or dz < ndsm_min:
-        outlier.append(i)
-        continue    # skip nodata 
-    xyz[2] = dz
+if not pc.has_points():
+    print(f'Point cloud file not found or empty {pc_filename}')
+    sys.exit(2)
+ndsm(src_ds, pc)
 # write out normalized point cloud (elevation from ground)
-if len(outlier) > 0:
-    pc = pc.select_by_index(outlier, invert=True)
 o3d.io.write_point_cloud(out_filename, pc)

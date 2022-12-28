f544184185fceee1483804f4e77d0afbfa2ac9d1
zsiki <siki1958@gmail.com>
2022-12-28 09:18:01 +0100

    updated

--- python/process.sh
+++ python/process.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# parameters for plane segment
 if [[ $# -eq 0 ]]
 then
     echo Usage: $0 point_cloud [working_directory]
@@ -18,8 +19,9 @@ echo $bn1
 echo $dir
 # create DEM
 echo ---- pc2dem ----
-echo ./pc2dem.py -r 1 -o $dir/$bn1 --rigidness 3 $pc
-./pc2dem.py -r 1 -o $dir/$bn1 --rigidness 3 $pc
+RIGIDNESS=3
+echo ./pc2dem.py -r 1 -o $dir/$bn1 --rigidness $RIGIDNESS $pc
+./pc2dem.py -r 1 -o $dir/$bn1 --rigidness $RIGIDNESS $pc
 if [[ $? -ne 0 ]]
 then
     echo pc2dem.py fails
@@ -27,8 +29,9 @@ then
 fi
 # create nDSM
 echo ---- pc2ndsm ----
-echo ./pc2ndsm.py $dir/$bn1.tif $pc 0.3
-./pc2ndsm.py $dir/$bn1.tif $pc 0.3
+MIN_HEIGHT=0.3
+echo ./pc2ndsm.py $dir/$bn1.tif $pc $MIN_HEIGHT
+./pc2ndsm.py $dir/$bn1.tif $pc $MIN_HEIGHT
 if [[ $? -ne 0 ]]
 then
     echo pc2ndsm.py fails
@@ -43,18 +46,23 @@ then
     echo plane_segment.py fails
     exit
 fi
+# alternate wall separation by normals OVERWRITES WALLS CREATED BY plane_segment
+echo ./wall_segment.py -o $dir $dir/${bn1}_ndsm.ply
+./wall_segment.py -o $dir $dir/${bn1}_ndsm.ply 
 # cluster roofs
 echo ---- dbscan_clustering ----
-echo ./dbscan_clustering.py --eps 2 --min_points 100 --folder $dir/roofs $dir/${bn1}_ndsm_roof.ply
-./dbscan_clustering.py --eps 2 --min_points 100 --folder $dir/roofs $dir/${bn1}_ndsm_roof.ply
+MIN_POINTS=200
+echo ./dbscan_clustering.py --eps 2 --min_points $MIN_POINTS --folder $dir/roofs $dir/${bn1}_ndsm_roof.ply
+./dbscan_clustering.py --eps 2 --min_points $MIN_POINTS --folder $dir/roofs $dir/${bn1}_ndsm_roof.ply
 if [[ $? -ne 0 ]]
 then
     echo dbscan_clustering.py fails
     exit
 fi
 echo ---- clusters2buildings ----
-echo ./clusters2buildings.py -c $dir/roofs -w $dir/walls -a 12 $dir/${bn1}_ndsm_wall.ply
-./clusters2buildings.py -c $dir/roofs -w $dir/walls -a 12 $dir/${bn1}_ndsm_wall.ply
+A=20
+echo ./clusters2buildings.py -c $dir/roofs -w $dir/walls -a $A $dir/${bn1}_ndsm_wall.ply
+./clusters2buildings.py -c $dir/roofs -w $dir/walls -a $A $dir/${bn1}_ndsm_wall.ply
 echo ---- edges ----
 echo ./edges.py $dir/walls/walls_*.ply
 ./edges.py --debug $dir/walls/walls_*.ply
